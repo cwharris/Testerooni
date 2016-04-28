716f4e57c243688ec72d0b6f206855a402eda1b3
Christopher Harris <xixonia@gmail.com>
2016-04-28 15:57:30 -0500

    .

---
+++ src/Testerooni/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Testerooni.Controllers
+{
+    [Route("api/test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public void TestGet()
+        {
+            var request = Request;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public void TestPost()
+        {
+            var request = Request;
+        }
+    }
+}
